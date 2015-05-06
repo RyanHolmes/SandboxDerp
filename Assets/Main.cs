7f554cc68ebc51ba9997681373127ccee9ee6749
Ryan Holmes <rholme3@uwo.ca>
2015-05-06 17:42:55 -0400

    added the placement tool prefab

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class Main : MonoBehaviour {
 
 	public GameObject block;
+	public GameObject tool;
 
 	// Use this for initialization
 	void Start () {
 		GameObject t = (GameObject)Instantiate (block, new Vector3(1.1f, 2.2f, 4.5f), Quaternion.identity);
-		t.name = "fuck";	
+		t.name = "test";	
+		GameObject g = (GameObject)Instantiate (tool, new Vector3(1,1,1), Quaternion.identity);
+		g.name = "tool";
 	}
 	
 	// Update is called once per frame
