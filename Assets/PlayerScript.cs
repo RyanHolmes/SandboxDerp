a75a0fccb810e01985fe23555e60917ecec0f230
Ryan Holmes <rholme3@uwo.ca>
2015-05-05 20:10:34 -0400

    For hannes <3

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Camera cam;
+	public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate(cam.transform.forward);
+			Debug.Log(this.transform.forward.ToString());
+		}
+		else if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate(-cam.transform.forward);
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Rotate(Vector3.up * Time.deltaTime * 100);
+		}
+		else if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Rotate(Vector3.down * Time.deltaTime * 100);
+		}
+	}
+}
